c97f0fdb24c9e85bb141f00bd59096c978ea9693
Stefan Plazic <stefan.plazic@gmail.com>
2019-10-03 08:48:28 +0200

    Adding procfile for heroku

--- build/index.js
+++ build/index.js
@@ -1,36 +1,47 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoSanitize from 'express-mongo-sanitize';
-import path from 'path';
-import logger from 'morgan';
+'use strict';
 
-const app = express();
-const port = process.env.PORT || 3000;
+var _express = require('express');
 
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-/*app.use(cors());
-app.use(mongoSanitize());
-app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    next();
-});*/
-app.use(express.static(path.join(__dirname, 'public')));
+var _express2 = _interopRequireDefault(_express);
 
-app.get('/', (req, res) => {
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _expressMongoSanitize = require('express-mongo-sanitize');
+
+var _expressMongoSanitize2 = _interopRequireDefault(_expressMongoSanitize);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _morgan = require('morgan');
+
+var _morgan2 = _interopRequireDefault(_morgan);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+var port = process.env.PORT || 3000;
+
+app.use((0, _morgan2.default)('dev'));
+app.use(_bodyParser2.default.urlencoded({ extended: true }));
+app.use(_bodyParser2.default.json());
+
+app.use((0, _expressMongoSanitize2.default)());
+
+app.use(_express2.default.static(_path2.default.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
     res.json({ "tutorial": "Build REST API with node.js" });
 });
 
 /*create a test webhook*/
 // Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {
+app.post('/webhook', function (req, res) {
 
-    let body = req.body;
+    var body = req.body;
 
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
@@ -40,7 +51,7 @@ app.post('/webhook', (req, res) => {
 
             // Gets the message. entry.messaging is an array, but 
             // will only ever contain one message, so we get index 0
-            let webhook_event = entry.messaging[0];
+            var webhook_event = entry.messaging[0];
             console.log(webhook_event);
         });
 
@@ -50,18 +61,17 @@ app.post('/webhook', (req, res) => {
         // Returns a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
     }
-
 });
 
-app.get('/webhook', (req, res) => {
+app.get('/webhook', function (req, res) {
 
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "watterBot"
+    var VERIFY_TOKEN = "ranstr";
 
     // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['challenge'];
+    var mode = req.query['hub.mode'];
+    var token = req.query['hub.verify_token'];
+    var challenge = req.query['challenge'];
 
     // Checks if a token and mode is in the query string of the request
     if (mode && token) {
@@ -72,7 +82,6 @@ app.get('/webhook', (req, res) => {
             // Responds with the challenge token from the request
             console.log('WEBHOOK_VERIFIED');
             res.status(200).send(challenge);
-
         } else {
             // Responds with '403 Forbidden' if verify tokens do not match
             res.sendStatus(403);
@@ -89,8 +98,10 @@ app.use(function (err, req, res, next) {
     res.send(err);
     //save errors to loger
     console.error(err);
-
 });
 
 //listen to the port
-app.listen(port, () => { console.log('Node server listening on port 3000'); });
+app.listen(port, function () {
+    console.log('Node server listening on port 3000');
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.js
@@ -0,0 +1,90 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import mongoSanitize from 'express-mongo-sanitize';
+import path from 'path';
+import logger from 'morgan';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(mongoSanitize());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.json({ "tutorial": "Build REST API with node.js" });
+});
+
+/*create a test webhook*/
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {
+
+    let body = req.body;
+
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function (entry) {
+
+            // Gets the message. entry.messaging is an array, but 
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+        });
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
+
+});
+
+app.get('/webhook', (req, res) => {
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "ranstr"
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
+});
+
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.send(err);
+    //save errors to loger
+    console.error(err);
+
+});
+
+//listen to the port
+app.listen(port, () => { console.log('Node server listening on port 3000'); });
